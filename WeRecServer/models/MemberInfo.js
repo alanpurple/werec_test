31f0e578d35f9d48781029ea5b10090269a45cc1
Alan Anderson <alanpurple@gmail.com>
2018-05-29 17:51:18 +0900

    add memberinfo

---
+++ WeRecServer/models/MemberInfo.js
@@ -0,0 +1,25 @@
+﻿const mongoose = require('../set-mongoose');
+const Schema = mongoose.Schema;
+
+const SubSchema1 = new Schema({
+    lid: Number,
+    ln1: String,
+    ln2: String
+}, { _id: false });
+
+const SubSchema2 = new Schema({
+    dvcid: String,
+    dn: String,
+    dot: Number,
+    dv: String,
+    av: String
+});
+
+const MemberInfoSchema = new Schema({
+    mid: Number,
+    s: Number,
+    bq: Number,
+    fl: [SubSchema1],
+    g: Number,
+    d: [SubSchema2]
+});
