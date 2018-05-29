c01f0938f5e86c42ce40c7eca1fd8a9a20659747
Alan Anderson <alanpurple@gmail.com>
2018-05-29 17:29:13 +0900

    update database schema

--- WeRecServer/models/DealInfo.js
+++ WeRecServer/models/DealInfo.js
@@ -1,1 +1,26 @@
-﻿
+﻿const mongoose = require('../set-mongoose');
+const Schema = mongoose.Schema;
+
+const DealInfoSchema = new Schema({
+    _id: Number,
+    _class: String,
+    did: Schema.Types.ObjectId,
+    dgid: Number,
+    sst: Date,
+    set: Date,
+    pe: Number,
+    po: Number,
+    pot: Number,
+    ps: Number,
+    st: Number,
+    ef: Number,
+    lttd: String,
+    lgtd: String,
+    ci: String,
+    mn: String,
+    ls: String,
+    sn: String,
+    on: String,
+    dc: String,
+    t: Object
+});
--- WeRecServer/models/MemberActionInfo.js
+++ WeRecServer/models/MemberActionInfo.js
@@ -1,1 +1,25 @@
-﻿
+﻿const mongoose = require('../set-mongoose');
+const Schema = mongoose.Schema;
+
+const SubSchema1 = new Schema({
+    did: { type: Number, ref: 'DealInfo' },
+    s: String
+}, { _id: false });
+
+const SubSchema2 = new Schema({
+    did: { type: Number, ref: 'DealInfo' },
+    oid: Number,
+    cnt: Number
+}, { _id: false });
+
+MemberActionInfoSchema = new Schema({
+    mid: Number,
+    day: String,
+    ft: {
+        type: {
+            s: [String],
+            c: [SubSchema1],
+            o: [SubSchema2]
+        }
+    }
+});
