074f1f318414e2317e99bdd8490c098c75093bb7
Alan Anderson <alanpurple@gmail.com>
2018-08-13 11:27:10 +0900

    add option to mongoose connection

--- WeRecServer/connection-praha.js
+++ WeRecServer/connection-praha.js
@@ -1,3 +1,3 @@
 ﻿const mongoose = require('./set-mongoose');
-const opts = { user: 'praha_read', pass: 'praha!@#' };
+const opts = { user: 'praha_read', pass: 'praha!@#', useNewUrlParser: true };
 module.exports = mongoose.createConnection('35.190.239.204', 'praha', 27017, opts);
