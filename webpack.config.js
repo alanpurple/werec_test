c4be6937b8765a480cb85574f86f50b20e259d9d
Alan Anderson <alanpurple@gmail.com>
2023-01-03 10:06:37 +0900

    update packages

--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-﻿const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const AotPlugin = require('@ngtools/webpack').AotPlugin;
-const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
